aeb9befd3eefe3cf54be7945e2f2f0346328f222
Akanksha Saran <akankshasaran@gmail.com>
2018-09-16 21:02:53 -0500

    Gaze coordinates synced with json data

--- record_demonstration_with_eye_tracker/scripts/sync_gaze_offset.py
+++ record_demonstration_with_eye_tracker/scripts/sync_gaze_offset.py
@@ -1,24 +1,7 @@
+from numpy import ones,vstack
+from numpy.linalg import lstsq
 import cv2
 import ast 
-
-vidcap = cv2.VideoCapture('../data/fullstream.mp4')
-fps = vidcap.get(cv2.CAP_PROP_FPS)
-print fps 	#25 fps
-success,image = vidcap.read()
-
-count = 0
-imgs = []       # list of image frames
-frame2ts = []   # corresponding list of video time stamp values in microseconds
-success = True
-while success:
-	#cv2.imwrite("data/imgs/%d.jpg" % count, image)     # save frame as JPEG file   
-	#imgs.append(image)   
-	success,image = vidcap.read()
-	frame2ts.append(int((count/fps)*1000000))
-	#print('Read a new frame: ', success)
-	count += 1
-	#print(count)
-print('read', str(count),'image frames')
 
 with open ("../data/livedata.json", "r") as myfile:
 	data=myfile.readlines()
@@ -51,11 +34,18 @@ for d in data:
 		gp[d['ts']] = d['gp']   #list of 2 coordinates
 print('read json')
 
-RE_pd_list = sorted(right_eye_pd.iterkeys())
-LE_pd_list = sorted(left_eye_pd.iterkeys())
+# map vts to ts
+all_vts = sorted(vid2ts.keys())
+a = all_vts[0]
+model = []
+for i in range(1,len(all_vts)):
+	points = [(a,vid2ts[a]),(all_vts[i],vid2ts[all_vts[i]])]
+	x_coords, y_coords = zip(*points)
+	A = vstack([x_coords,ones(len(x_coords))]).T
+	m, c = lstsq(A, y_coords, rcond=None)[0]
+	print("Line Solution is ts = {m}vts + {c}".format(m=m,c=c))
+	model.append((m,c))
 
-gp_list = sorted(gp.iterkeys())
-print(len(gp_list))    # Total Number of gaze points detected during the video (sampling rate 50Hz)
 
 from bisect import bisect_left
 def takeClosest(myList, myNumber):
@@ -76,32 +66,44 @@ def takeClosest(myList, myNumber):
 	else:
 	   return before
 
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-#out = cv2.VideoWriter('./videos/001_output.mp4',fourcc, 29.0, size, False)
-video = cv2.VideoWriter('video.avi',fourcc,fps,(1920,1080))
 
 vidcap = cv2.VideoCapture('../data/fullstream.mp4')
-success,img = vidcap.read()
-i = 0
-while success:
-	
-	#for i,img in enumerate(imgs):
-	print(i)
-	ts = frame2ts[i]
-	all_tracker_ts = sorted(vid2ts.keys())
-	#print(all_tracker_ts)
-	print(ts)
-	ts_pos = takeClosest(all_tracker_ts, ts)
-	print(ts_pos)
-	#print(vid2ts)
-	#if(vid2ts[0] in gp_list):
-	#	gaze = gp[vid2ts[all_tracker_ts[ts_pos]]]
-	gaze = gp[gp_list[i]]
-	print(gaze)
+fps = vidcap.get(cv2.CAP_PROP_FPS)
+print fps 	#25 fps
+success, img = vidcap.read()
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+video = cv2.VideoWriter('gaze.avi',fourcc,fps,(1920,1080))
+
+all_ts = sorted(gp.keys())
+count = 0
+imgs = []       # list of image frames
+frame2ts = []   # corresponding list of video time stamp values in microseconds
+while success:	
+	print(count)
+	frame_ts = int((count/fps)*1000000)
+	frame2ts.append(frame_ts)
+	#print('Read a new frame: ', success)
+	#print('frame_ts: ', frame_ts)
+	#print(all_vts)
+	less = [a for a in all_vts if a<=frame_ts]
+	idx = len(less)-1
+	print(idx)
+	# start = less[-1]
+	if idx<len(model):
+		m,c = model[idx]
+	else:
+		m,c = model[len(model)-1]
+	ts = m*frame_ts + c
+	#print('ts: ',ts)
+	tracker_ts = takeClosest(all_ts,ts)
+	#print('tracker_ts_pos: ', tracker_ts)
+	gaze = gp[tracker_ts]
 	cv2.circle(img,(int(gaze[0]*1920), int(gaze[1]*1080)), 5, (0,255,0), -1)
 	video.write(img)
-	i = i + 1
-	success,img = vidcap.read()
-	print(success)
+
+	count += 1
+	success, img = vidcap.read()
+
 cv2.destroyAllWindows()
 video.release()
