310c3baac51c6679f27431e3c9e0cb4632ad1aa3
Akanksha Saran <akankshasaran@gmail.com>
2018-12-26 16:13:45 -0600

    BIRL experiments with gaze incorporate into P(R|D)

---
+++ record_demonstration_with_eye_tracker/scripts/birl/GazeStudyTest.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python2
+
+'''This code tests out using rbfs around each object. The correct reward is to place it to the left of a the spoon (object 0)'''
+
+import random
+import numpy as np
+import birl
+import active_utils as autils
+import matplotlib.pyplot as plt
+import active_var_complexreward as active_var
+import sys
+
+from copy import deepcopy
+
+def test_placements(true_reward, num_test):
+    test_rbfs = []
+    for i in range(num_test):
+        #generate new centers, but keep weights
+        num_objs = true_reward.num_objects
+        new_centers = np.random.rand(num_objs, 2)
+        obj_weights = true_reward.obj_weights.copy()
+        abs_weights = true_reward.abs_weights.copy()
+        new_rbf = autils.RbfComplexReward(new_centers, obj_weights, abs_weights)
+        test_rbfs.append(new_rbf)
+        #autils.visualize_reward(new_rbf, "test placement")
+    return test_rbfs
+
+
+#return mean and standard deviation of loss over test placements
+def calc_test_reward_loss(test_placements, map_params, visualize=False):
+    losses = []
+    for placement in test_placements:
+        test_config = placement.obj_centers
+        true_params = (placement.obj_weights, placement.abs_weights)
+        ploss = active_var.calculate_policy_loss(test_config, true_params, map_params)
+        if visualize:
+            test_map = autils.RbfComplexReward(test_config, map_params[0], map_params[1])
+            autils.visualize_reward(test_map, "testing with map reward")
+            plt.show()
+        losses.append(ploss)
+    losses = np.array(losses)
+    return np.mean(losses), np.std(losses), np.max(losses)
+
+
+def calc_test_placement_loss(test_placements, map_params, visualize=False):
+    losses = []
+    cnt = 0
+    for placement in test_placements:
+        #print cnt
+        cnt += 1
+        test_config = placement.obj_centers
+        true_params = (placement.obj_weights, placement.abs_weights)
+        ploss = active_var.calculate_placement_loss(test_config, true_params, map_params)
+        if visualize:
+            test_map = autils.RbfComplexReward(test_config, map_params[0], map_params[1])
+            autils.visualize_reward(test_map, "testing with map reward")
+            plt.show()
+        losses.append(ploss)
+    losses = np.array(losses)
+    return np.mean(losses), np.std(losses), np.max(losses)
+
+
+if __name__=="__main__":
+
+    rand_seed = 12345
+    np.random.seed(rand_seed)
+
+    beta=100.0
+    num_steps = 1000
+    step_std = 0.05
+    burn = 0
+    skip = 25
+    num_test = 100
+
+    exp = 'bowl' # or 'bowl'
+
+    num_objects = 2 #plate and bowl
+    #object weights are for center, top left, top right, bottom left, bottom right
+    if exp=='bowl':
+        obj1_weights = np.array([0.0, 0.5, 0.0, 0.5, 0.0]) #bowl: equal weight on top left and bottom left rbf results in placement directly to left of object
+        obj0_weights = np.array([0.0, 0.0, 0.0, 0.0, 0.0]) #plate
+
+        demo_plate = [[0.437,0.668],[0.421,0.665],[0.428,0.459],[0.436,0.77], [0.425,0.777]]
+        demo_bowl = [[0.588,0.605],[0.587,0.609],[0.591,0.396],[0.598, 0.711], [0.594,0.724]]
+        demo_spoon = [[0.552,0.663],[0.536,0.638],[0.551,0.42], [0.532,0.738], [0.549,0.731]]
+        demo_gaze = [[6.854,13.306],[1.96,42.156],[0.0,49.253], [0.0,24.58], [0.0,13.978]]
+    #obj2_weights = np.array([0.0, 0.0, 0.0, 0.0, 0.0]) #distractor
+    elif exp=='plate':
+        obj1_weights = np.array([0.0, 0.0, 0.0, 0.0, 0.0]) #bowl: equal weight on top left and bottom left rbf results in placement directly to left of object
+        obj0_weights = np.array([0.0, 0.0, 0.5, 0.0, 0.5]) #plate
+
+        demo_plate = [[0.438,0.670],[0.428,0.657],[0.427,0.461],[0.435,0.761],[0.423,0.777]]
+        demo_bowl = [[0.591,0.611],[0.582,0.604],[0.508,0.435],[0.598,0.715],[0.593,0.720]]
+        demo_spoon = [[0.507, 0.638],[0.514,0.641],[0.508,0.435],[0.532,0.738],[0.495,0.746]]
+        demo_gaze = [[23.42,5.948],[10.577,6.73],[13.861,0.0],[20.382,0.0],[30.0,0.0]]
+
+    obj_weights = np.concatenate((obj0_weights, obj1_weights))
+    abs_weights = np.array([0.0, 0.0, 0.0,
+                            0.0, 0.0, 0.0,
+                            0.0, 0.0, 0.0]) # no absolute placement preferences
+    
+    num_obj_weights = len(obj_weights)
+    num_abs_weights = len(abs_weights)
+
+    birl = birl.BIRL(num_obj_weights, num_abs_weights, beta, num_steps, step_std, burn, skip)
+
+    #give three demos in different positions
+    num_demos = 5
+
+
+    for i in range(num_demos):
+    # for i in [2,3,4]:
+        #generate random object placements
+        # obj_centers += np.random.random((num_objects,2))*0.01
+        # demo_rbf = autils.RbfComplexReward(obj_centers, obj_weights, abs_weights)
+        # best_x, reward = demo_rbf.estimate_best_placement()
+        # best_x = np.array([0.5, 0.5]) + np.random.random(2)*0.1
+
+        obj_centers = np.array([demo_plate[i],demo_bowl[i]])
+        look_times = np.array(demo_gaze[i])
+        best_x = np.array(demo_spoon[i]) 
+        true_rbf = autils.RbfComplexReward(obj_centers, obj_weights, abs_weights)
+
+        birl.add_gaze_demonstration(obj_centers, best_x, look_times)
+
+        print "demo", best_x
+        autils.visualize_reward(true_rbf, "demo {} and ground truth reward".format(i) )
+    #plt.show()
+
+
+    #run birl to get MAP estimate
+    birl.run_inference(gaze=True)
+    # birl.run_gaze_inference()
+
+
+    #print out the map reward weights
+    map_obj_wts, map_abs_wts = birl.get_map_params()
+    # print "obj weights", map_obj_wts
+    # print "abs weights", map_abs_wts
+
+    # test_file = "./data/user_study_gaze" + str(rand_seed) + "_random.txt"
+    test_file = "./data/user_study_gaze.txt"
+    f = open(test_file,"w")
+    print f
+    
+    # true_rbf = autils.RbfComplexReward(obj_centers, obj_weights, abs_weights)
+    print(true_rbf.obj_weights)
+    print(obj_weights)
+    test_rbfs = test_placements(true_rbf, num_test)
+    mean_obj_wts, mean_abs_wts = birl.get_map_params()
+    print "obj weights", mean_obj_wts
+    print "abs weights", mean_abs_wts
+    ave_loss, std_loss, max_loss = calc_test_reward_loss(test_rbfs, birl.get_map_params(), False)
+    f.write("policy loss,{},{},{}\n".format(ave_loss, std_loss, max_loss))
+    print "policy loss:", ave_loss, std_loss, max_loss
+
+    ave_loss, std_loss, max_loss = calc_test_placement_loss(test_rbfs, birl.get_map_params(), False)
+    f.write("placement loss,{},{},{}\n".format(ave_loss, std_loss, max_loss))
+    print "placement loss:", ave_loss, std_loss, max_loss
+    print "reward diff:", np.linalg.norm(true_rbf.obj_weights - mean_obj_wts), np.linalg.norm(true_rbf.abs_weights - mean_abs_wts)
+    f.close()
+
+    #create rbf for MAP reward found by BIRL in test configurations to see how it generalizes
+    for i in range(3):
+        #I'm giving it random object placements to see if it generalizes
+        rand_obj_centers = np.random.random((num_objects,2))
+        map_reward_rbf = autils.RbfComplexReward(rand_obj_centers, map_obj_wts, map_abs_wts)
+
+        #visualize learned reward and optimal placement
+        autils.visualize_reward(map_reward_rbf, title="test {} for MAP reward".format(i))
+    # plt.show()
---
+++ record_demonstration_with_eye_tracker/scripts/birl/birl.py
@@ -0,0 +1,299 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 10 11:18:07 2018
+#uses absolute and relative RBFs to create reward function
+@author: dsbrown
+"""
+import active_utils
+import copy
+import numpy as np
+import matplotlib.pyplot as plt
+#import profile
+import scipy.misc
+
+
+#normalizes the reward weights so l1 norm is 1
+#uses softmax sampling to approximate the partition function
+
+class BIRL():
+    def __init__(self, num_obj_weights, num_abs_weights, beta=10.0, num_steps=100, step_std=0.2, burn=50, skip=10):
+        self.demonstrations = []
+        self.beta = beta
+        self.num_steps = num_steps
+        self.step_std = step_std
+        self.burn = burn
+        self.skip = skip
+        self.num_obj_weights = num_obj_weights
+        self.num_abs_weights = num_abs_weights
+        self.mcmc_chain = []
+        self.demonstrations = []
+        self.map_obj_weights = None
+        self.map_abs_weights = None
+        self.init_obj_weights = np.zeros(self.num_obj_weights)
+        self.init_abs_weights = np.zeros(self.num_abs_weights)
+        self.run_before = False #if mcmc has been run before we should seed the MCMC chain with the map from last time
+        
+    ''' a demo is a list of object centroids and an (x,y) placement'''
+    def add_demonstration(self, centroids, placement):
+        self.demonstrations.append((centroids,placement))
+        
+    ''' a demo is a list of object centroids, an (x,y) placement and gaze fixation time per object'''
+    def add_gaze_demonstration(self, centroids, placement, look_times):
+        self.demonstrations.append((centroids,placement,look_times))
+        
+    def run_inference(self, gaze=False):
+        #either randomly seed or used MAP from previous iteration
+        if not self.run_before:
+            curr_obj_weights, curr_abs_weights = self.generate_proposal_weights(self.init_obj_weights, self.init_abs_weights)
+        else: 
+            curr_obj_weights, curr_abs_weights = self.get_map_params()
+    
+        self.mcmc_chain = []
+        print "running birl"
+        #first check if there are demos
+        assert(len(self.demonstrations) > 0)
+        #start with some random initial weights
+        #curr_obj_weights = 2.0*np.random.rand(self.num_obj_weights)-1.0
+        #curr_abs_weights = 2.0*np.random.rand(self.num_abs_weights)-1.0
+        
+        
+    
+        #compute log likelihood over demonstrations
+        if (not gaze):
+            curr_ll =  self.log_likelihood(curr_obj_weights, curr_abs_weights)
+        else:
+            curr_ll =  self.gaze_log_likelihood(curr_obj_weights, curr_abs_weights)
+            # print(curr_ll)
+        best_ll = -np.inf
+        
+        #run MCMC
+        accept_cnt = 0
+        for step in range(self.num_steps):
+            #print step
+            #compute proposal and log likelihood
+            prop_obj_weights, prop_abs_weights = self.generate_proposal_weights(curr_obj_weights, curr_abs_weights)
+            #print "proposal:"
+            #print prop_obj_weights
+            #print prop_abs_weights
+            if (not gaze):
+                prop_ll = self.log_likelihood(prop_obj_weights, prop_abs_weights)
+            else:
+                prop_ll = self.gaze_log_likelihood(prop_obj_weights, prop_abs_weights)
+            
+            # print(curr_ll,prop_ll)
+            prob_accept = min(1.0, np.exp(prop_ll - curr_ll))
+            if np.random.rand() < prob_accept:
+                accept_cnt += 1
+                #print "accept!!!!!!!!!!!!!!"
+                #accept and add to chain
+                self.mcmc_chain.append((prop_obj_weights.copy(), prop_abs_weights.copy()))
+                curr_ll = prop_ll
+                curr_obj_weights = prop_obj_weights
+                curr_abs_weights = prop_abs_weights
+                if prop_ll > best_ll:
+                    best_ll = prop_ll
+                    self.map_obj_weights = prop_obj_weights.copy()
+                    self.map_abs_weights = prop_abs_weights.copy()
+            else:
+                self.mcmc_chain.append((curr_obj_weights.copy(), curr_abs_weights.copy()))
+                
+        #update init weights to map of last run
+        self.init_obj_weights, self.init_abs_weights = self.get_map_params()
+        print "accepted", accept_cnt
+        
+    def get_mcmc_chain(self):
+        return self.mcmc_chain
+        
+    def get_map_params(self):
+        return self.map_obj_weights, self.map_abs_weights
+        
+    def get_mean_params(self):
+        mean_obj = np.zeros(self.num_obj_weights)
+        mean_abs = np.zeros(self.num_abs_weights)
+        for i in range(self.burn, len(self.mcmc_chain), self.skip):
+            r_obj, r_abs = self.mcmc_chain[i]
+            mean_obj += r_obj
+            mean_abs += r_abs
+            #print i, "-----"
+            #print r_obj
+            #print r_abs
+
+        #normalize
+        weight_l1_norm = 0
+        for w1 in mean_obj:
+            weight_l1_norm += np.abs(w1)
+        for w2 in mean_abs:
+            weight_l1_norm += np.abs(w2)
+        for i in range(len(mean_obj)):
+            mean_obj[i] /= weight_l1_norm
+        for i in range(len(mean_abs)):
+            mean_abs[i] /= weight_l1_norm            
+        
+        return mean_obj, mean_abs
+        
+    
+    def log_likelihood(self, hyp_obj_weights, hyp_abs_weights, granularity = 10):
+        #print hyp_obj_weights
+        #print hyp_abs_weights
+        log_sum = 0.0
+        for centers, placement, gaze in self.demonstrations:
+            #to compute the optimal placement under the demo config
+            #make new rbf with hypothesis weights and widths
+            hyp_rbf = active_utils.RbfComplexReward(centers, hyp_obj_weights, hyp_abs_weights)
+            #active_utils.visualize_reward(hyp_rbf, title="birl hyp")
+            placement_reward = hyp_rbf.get_reward(placement)
+                      
+            xspace = np.linspace(0,1,granularity)
+            yspace = np.linspace(0,1,granularity)
+            pairs = [(xi,yi) for xi in xspace for yi in yspace]
+            #for p in pairs:
+            #    plt.plot(p[0],p[1],'x')
+            
+            # print pairs
+            Z_exponents = []
+            for pair in pairs:
+                Z_exponents.append(self.beta * hyp_rbf.get_reward(pair))
+            #print Z_exponents
+            log_sum += self.beta * placement_reward - scipy.misc.logsumexp(Z_exponents)
+            #print "likelihood:", np.exp(self.beta * placement_reward - scipy.misc.logsumexp(Z_exponents))
+            #plt.show()
+        return log_sum
+
+
+
+    def gaze_log_likelihood(self, hyp_obj_weights, hyp_abs_weights, granularity = 10):
+        #print hyp_obj_weights
+        #print hyp_abs_weights
+        log_sum = 0.0
+        for centers, placement, gaze in self.demonstrations:
+            #to compute the optimal placement under the demo config
+            #make new rbf with hypothesis weights and widths
+            hyp_rbf = active_utils.RbfComplexReward(centers, hyp_obj_weights, hyp_abs_weights)
+            #active_utils.visualize_reward(hyp_rbf, title="birl hyp")
+            placement_reward = hyp_rbf.get_reward(placement)
+                      
+            xspace = np.linspace(0,1,granularity)
+            yspace = np.linspace(0,1,granularity)
+            pairs = [(xi,yi) for xi in xspace for yi in yspace]
+            #for p in pairs:
+            #    plt.plot(p[0],p[1],'x')
+            
+            # print pairs
+            Z_exponents = []
+            for pair in pairs:
+                Z_exponents.append(self.beta * hyp_rbf.get_reward(pair))
+            #print Z_exponents
+
+            # gaze term
+            # for i in range(2):
+            if sum(hyp_obj_weights)!= 0:
+                o1w = sum(hyp_obj_weights[:5])/(sum(hyp_obj_weights))
+                o2w = sum(hyp_obj_weights[5:])/(sum(hyp_obj_weights))
+            else:
+                o1w = 1000000
+                o2w = 1000000
+            l1 = gaze[0]/(gaze[0]+gaze[1])
+            l2 = gaze[1]/(gaze[0]+gaze[1])
+            if(l1==0):
+                l1 = 1000000
+            if(l2==0):
+                l2 = 1000000
+            # gaze_term = abs(l1/l2 - o1w/o2w) + abs(l2/l1 - o2w/o1w) 
+            gaze_term = (o2w>o1w and l2<l1)*(l1/l2) + (o1w>o2w and l1<l2)*(l2/l1)
+            # print('gaze_term: ',gaze_term,o1w,o2w,l1,l2, gaze[0], gaze[1])
+
+            log_sum += self.beta * placement_reward - scipy.misc.logsumexp(Z_exponents) + gaze_term
+            #print "likelihood:", np.exp(self.beta * placement_reward - scipy.misc.logsumexp(Z_exponents))
+            #plt.show()
+            # print('log_sum: ', log_sum)
+        return log_sum
+        
+    #generate normalized weights sum L1 =1
+    def generate_proposal_weights(self, weights1, weights2):
+        # print('weights1: ', weights1)
+        # print('weights2: ', weights2)
+        new_weights1 = weights1.copy()
+        new_weights1 += np.random.normal(0, self.step_std, weights1.shape)
+        new_weights2 = weights2.copy()
+        new_weights2 += np.random.normal(0, self.step_std, weights2.shape)
+        
+        #normalize
+        weight_l1_norm = 0
+        for w1 in new_weights1:
+            weight_l1_norm += np.abs(w1)
+        for w2 in new_weights2:
+            weight_l1_norm += np.abs(w2)
+        for i in range(len(new_weights1)):
+            new_weights1[i] /= weight_l1_norm
+        for i in range(len(new_weights2)):
+            new_weights2[i] /= weight_l1_norm
+            
+
+        return new_weights1, new_weights2
+
+
+        
+#    def generate_proposal_widths(self, widths):
+#        new_widths = widths.copy()
+#        new_widths += np.random.normal(0, self.step_std, widths.shape)
+#        #truncate at zero
+#        new_widths[new_widths < 0.0] = 0.001
+#            
+#        return new_widths
+        
+def run_two_obj_birl_test():
+    num_objs = 2
+    weights = 2.0*np.random.rand(num_objs)-1.0
+    widths = np.random.rand(num_objs)
+    centers = np.random.rand(num_objs,2)
+    print "true weights", weights
+    print "true widths", widths
+    
+    rbf = active_utils.RbfReward(centers, weights, widths)
+
+    rbf.plot_heat_map()
+    
+    best_x, reward = rbf.estimate_best_placement(plot=False)
+    plt.plot(best_x[0], best_x[1],'*',markersize=30)
+    print "best x", best_x, "best reward", reward
+    
+    #plot centers of objects
+    for c in centers:
+        plt.plot(c[0],c[1],'o',markersize=20)
+    plt.title("demonstration")
+    #plt.show()
+
+
+
+
+    birl = BIRL(num_objs)
+    birl.add_demonstration(centers, best_x)
+    birl.run_inference()
+    map_weights, map_widths = birl.get_map_params()
+    print "map weights", map_weights
+    print "map widths", map_widths
+    rbf_map = active_utils.RbfReward(centers, map_weights, map_widths)
+
+    rbf_map.plot_heat_map()
+    
+    map_x, reward = rbf_map.estimate_best_placement(plot=False)
+    plt.plot(map_x[0], map_x[1],'*',markersize=30)
+    #plot centers of objects
+    for c in centers:
+        plt.plot(c[0],c[1],'o',markersize=20)
+    plt.title("map estimate")
+    print "best x", map_x, "best reward", reward
+    plt.show()
+        
+if __name__=="__main__":
+    #run_two_obj_birl_test()
+    birl = BIRL(3, 4, beta=10.0, num_steps=100, step_std=0.1)
+    w1 = np.zeros(3)
+    w2 = np.zeros(4)
+    for i in range(3):
+        print "---------"
+        w1, w2 = birl.generate_proposal_weights(w1, w2)
+        print w1
+        print w2
+        print np.linalg.norm(np.append(w1,w2),1)
+        
